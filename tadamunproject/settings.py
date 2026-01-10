3b014c88160c94c236798b2b0c6909c179bed053
cheikh22 <cheikh.raby24@gmail.com>
2026-01-10 23:25:56 +0000

    db for Render

--- tadamunproject/settings.py
+++ tadamunproject/settings.py
@@ -77,16 +77,16 @@ WSGI_APPLICATION = 'tadamunproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
